06ebc770fd001567be9913e70ef3b64b19705360
Kris <kris@Kris.local>
2020-04-15 20:54:18 -0400

    Variable practice

--- Beginner Python/hello_world.py
+++ Beginner Python/hello_world.py
@@ -1,1 +1,10 @@
-print("Hello Python world!")
+#This line was added to make a string variable 
+message = "Hello python world again"
+
+print("Hello python world!")
+#This will print the string variable
+print(message)
+
+#Adding this and changing this to show it can be changed later on down the code
+message= "Welcome to the crash course"
+print(message)
