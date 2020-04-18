f2b255593f03f8b9d529aeeb35c7e39f56e3efde
Kris <kris@Kris.local>
2020-04-18 14:39:15 -0400

    Learning about strings

---
+++ Beginner Python/names.py
@@ -0,0 +1,9 @@
+#Declare a string
+name = "ada lovelace"
+
+#Use the .title method to capitalize the first letter in the first and last name
+print(name.title())
+
+#There are other methods that change all the characters to uppercase and lower case like below:
+print(name.upper())
+print(name.lower())
