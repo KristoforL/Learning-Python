abd774bd53b47c19630cd657a27aa83c4bdd39c0
Kris <kris@Kris.local>
2020-04-20 04:23:37 -0400

    FIgured out why my f-string was not working

--- Beginner Python/full_name.py
+++ Beginner Python/full_name.py
@@ -6,15 +6,16 @@ last_name = "lovelace"
 full_name = f"{first_name} {last_name}"
 
 print(full_name)
+print(f"Hello, {full_name.title()}!")
 
 #I am not using the f-string to show you how it prints
+#Also do no forget that this has changed the full_name string so it will not work with f-string pass this point
 full_name = "{first_name} {last_name}"
 
 #As you can tell this is what happens when you do not use the f-string format. It then prints the actual string in the quatation marks
 print(full_name)
 
 #This integrates using the f-string to format a string
-print(f"Hello, {full_name.title()}!")
+print(f"Hello, full_name.title()!")
 
-#I wonder what would happen if I just format the string
-print(f"full_name")
+#I figured out the last push. I was not reading the code from top to bottom and missed that the string was changed. ¯\_(ツ)_/¯
