2fe6c2ea16c82190c5bd9880f5edaa9692871e76
Kris <kris@Kris.local>
2020-04-21 01:52:08 -0400

    Try it yourself assignements

---
+++ Beginner Python/2-8number_eight.py
@@ -0,0 +1,7 @@
+#Practice numbers and operations 
+#Write an addition, subtraction, multiplication, and division operation that will result in the number
+
+print(4+4)
+print(10-2)
+print(4*2)
+print(8/1)
---
+++ Beginner Python/2-9favorite_number.py
@@ -0,0 +1,6 @@
+#Creating a message that includes the integer of my favorite number
+
+fav_number = 15
+
+#I do not fav_number in
+print("My favorite number is " + fav_number)
