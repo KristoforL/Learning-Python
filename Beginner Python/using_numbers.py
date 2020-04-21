841bad371220b79f2475fcdbe86e8e65e0e018cc
Kris <kris@Kris.local>
2020-04-21 01:22:01 -0400

    more concepts

--- Beginner Python/using_numbers.py
+++ Beginner Python/using_numbers.py
@@ -14,9 +14,19 @@ print(3**2)
 print(3**3)
 print(10**6)
 
+print("\n")
+
 #Floats will be the default result if the equation includes floats
 #All these will result in a float
 print(.1 + .1)
 print(.2 + .2)
 print(2 *.1)
 print(2* .2)
+
+#Sometimes there will be a lot of unnecessary zeroes but they will be printed out per python programming
+print("\n")
+
+#You can use underscores in integers and when they are printed they will exclude the underscore and display the normal
+
+universal_age = 14_000_000_000
+print(universal_age)
