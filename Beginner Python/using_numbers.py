2fe9dec3a8473c5c35f52f1b3eecd1a7b5a1a828
Kris <kris@Kris.local>
2020-04-21 00:55:32 -0400

    numbers and operations

---
+++ Beginner Python/using_numbers.py
@@ -0,0 +1,22 @@
+#This is a script that will cover some good points in when dealing with numbers
+
+#You have to print these or they will not show in the console.
+#Basic operations
+print(2+3)
+print(3-2)
+print(2*3)
+print(3/2)
+
+print("\n")
+
+#This is using the exponents
+print(3**2)
+print(3**3)
+print(10**6)
+
+#Floats will be the default result if the equation includes floats
+#All these will result in a float
+print(.1 + .1)
+print(.2 + .2)
+print(2 *.1)
+print(2* .2)
