632caddc910cdeead157b06592b8baed1a097d2e
Kris <kris@Kris.local>
2020-04-21 11:53:30 -0400

    Working on list

---
+++ Beginner Python/bicycles.py
@@ -0,0 +1,8 @@
+#This is to start covering the list and how to declare them
+#list are declared with a variable name and the [] brackets with each element in it seperated by a comma
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+
+#You can print all content of the list by just printing the list name
+
+print(bicycles)
