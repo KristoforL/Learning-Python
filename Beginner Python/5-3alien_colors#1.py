5a007b55644fcf291d8ee303b3e2923c918a3078
Kris <kris@Kris.local>
2020-05-06 12:27:05 -0400

    Conditional statement practice

---
+++ Beginner Python/5-3alien_colors#1.py
@@ -0,0 +1,10 @@
+#Going to practice if statements during this assingment
+
+alien_color = 'green'
+
+
+if alien_color == 'green':
+    print("You just earned 5 points")
+
+if alien_color == 'red':
+    print("You just earned 5 points")
---
+++ Beginner Python/5-4alien_colors#2.py
@@ -0,0 +1,14 @@
+#Continuing the laast practice anad using anad if else statement
+
+alien_color = 'red'
+
+if alien_color == 'red':
+    print("You just earned 5 points!")
+else:
+    print("You just earned 10 pints!")
+    
+
+if alien_color != 'red':
+    print("You just earned 5 points!")
+else:
+    print("You just earned 10 pints!")
