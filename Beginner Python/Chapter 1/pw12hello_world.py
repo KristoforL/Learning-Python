7f83a3b614b28546fc82caea632232c3bb47034d
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-06-25 02:19:32 -0400

    Chapter 1 files

---
+++ Beginner Python/Chapter 1/pw12hello_world.py
@@ -0,0 +1,14 @@
+#This line was added to make a string variable 
+message = "Hello python world again"
+
+print("Hello python world!")
+#This will print the string variable
+print(message)
+
+#Adding this and changing this to show it can be changed later on down the code
+message= "Welcome to the crash course"
+print(message)
+
+#How to read stack traces and errors
+print(mesage)
+#It will generate an error thst tells you the file and the line it is located on. Then it will describe the error. In this case 'mesage' is not defined
