1a9f8839666f5874e26778bbbe86b065362a3a0a
Kris <kris@Kris.local>
2020-06-25 01:54:35 -0400

    delete

---
+++ Beginner Python/time_testing.py
@@ -0,0 +1,18 @@
+#import the time library as this is built in to the python library
+#I got this from python module of the week
+
+import textwrap
+import time
+
+#time.time()
+#This will print the number of seconds since the epoch
+print(f"time.time()")
+
+#time.ctime()
+#This will print off the calendar date and time
+print(f"The time is {time.ctime()}")
+#.clock has been deprecated 
+#print(f"{time.clock()}")
+
+
+
